79e3efb4b7f90579c9bfef47c27514418164b24e
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-05-08 20:46:12 +0700

    case 5

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -151,3 +151,36 @@ private:
                 break;
 
             case '3':
+            mhs.traverse();
+                break;
+
+            case '4':
+                if (mhs.listEmpty() == true)
+                {
+                    cout << "\nList Kosong\n";
+                    break;
+                }
+                Node *previous, *current;
+                cout << endl;
+                cout << "Masukkan no mahasiswa yang dicari : ";
+                cin >> nim;
+                if (mhs.Search(nim, &previous, ¤t) == false)
+                    cout << endl
+                         << "Data tidak ditemukan" << endl;
+                else
+                {
+                    cout << endl
+                         << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                }
+                break;
+
+            case '5':
+                break;
+
+            default:
+                cout << "Pilihan salah !" << endl;
+                break;
+            }
+        } while (ch != '5');
+
