e23b6986d053e4c29dd6dd16f307ff28f3039558
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-05-08 20:43:26 +0700

    delnode

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -76,3 +76,17 @@ private:
 
         return (*current != NULL);
     }
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, ¤t))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
