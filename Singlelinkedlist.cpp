b0a0b3bc76ff94b97b3a81b1b65dab7e0b2d09c8
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-05-08 20:45:33 +0700

    case 3

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -139,3 +139,15 @@ private:
                          << "List Kosong" << endl;
                     break;
                 }
+                cout << endl
+                     << "\nMasukkan no mahasiswa yang akan dihapus : ";
+                cin >> nim;
+                if (mhs.delNode(nim) == false)
+                    cout << endl
+                         << "Data tidak ditemukan" << endl;
+                else
+                    cout << endl
+                         << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+                break;
+
+            case '3':
