d065aa1e25a6b3ad5d459dfea9fc2f15a003feac
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-05-08 20:44:16 +0700

    int main bos

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -90,3 +90,30 @@ private:
         delete current;
         return true;
     }
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah:\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
+
+    int main()
+    {
+        LinkedList mhs;
+        int nim;
+        char ch;
+
+        do
+        {
